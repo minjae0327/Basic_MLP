fda6e7dde6dc9e62b76f13a3b2d6297e89fde840
minjae <minjae0327@gmail.com>
2022-12-08 12:23:14 +0900

    Message1

---
+++ util.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' ,')
+    words = text.split(' ')
+    
+    word_to_id = {}
+    id_to_word = {}
+
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+            
+    corpus = [word_to_id[w] for w in words]
+    corpus = np.array(corpus)
+    
+    return corpus, word_to_id, id_to_word
+
+def create_co_matrix(corpus, vocab_size, window_size = 1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_sizse, vocac_size), dfype = np.int32)
+    
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+            
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+                
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+                
+    return co_matrix
